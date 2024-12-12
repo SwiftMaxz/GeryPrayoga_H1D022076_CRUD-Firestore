14ec8ee8fbd20337956e5e030daae34cbc843c10
SwiftMaxz <gpnaxs283@gmail.com>
2024-12-12 15:47:30 +0700

    add screenshots

---
+++ src/utils/firebase.ts
@@ -0,0 +1,18 @@
+// src/utils/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA4oSVbe2jjjZgc5VQbqG6KPtcACBL4t_g",
+    authDomain: "vue-firebase-51a76.firebaseapp.com",
+    projectId: "vue-firebase-51a76",
+    storageBucket: "vue-firebase-51a76.firebasestorage.app",
+    messagingSenderId: "170185358092",
+    appId: "1:170185358092:web:c46d6f58d506fa9d572729"
+};
+
+const firebase = initializeApp(firebaseConfig);
+const auth = getAuth(firebase);
+const googleProvider = new GoogleAuthProvider();
+
+export { auth, googleProvider };
